2cfccb0969c99528b53c16326cb132330b1fd0d7
Dericioso <dery.lantigua@gmail.com>
2022-01-12 12:16:44 +0100

    Info to js added

---
+++ collection.js
@@ -0,0 +1,80 @@
+const COLLECTION = [{
+            name: 'Mario Bros',
+            director: 'Shigeru Miyamoto & Gunpei Yokoi',
+            releaseYear: 1983,
+            picture: 'link/to/a/picture',
+            platform: ['Arcade'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Super Mario Bros.',
+            director: 'Shigeru Miyamoto & Takashi Tezuka',
+            releaseYear: 1986,
+            picture: 'link/to/a/picture',
+            platform: ['Nintendo NES'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Super Mario World',
+            director: 'Shigeru Miyamoto and Takashi Tezuka',
+            releaseYear: 1990,
+            picture: 'link/to/a/picture',
+            platform: ['Super NES'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Super Mario Kart',
+            director: 'Shigeru Miyamoto',
+            releaseYear: 1992,
+            picture: 'link/to/a/picture',
+            platform: ['Super NES'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Super Mario 64',
+            director: 'Shigeru Miyamoto',
+            releaseYear: 1996,
+            picture: 'link/to/a/picture',
+            platform: ['Nintendo 64'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Luigi Mansion',
+            director: 'Hideki Konno',
+            releaseYear: 1994,
+            picture: 'link/to/a/picture',
+            platform: ['GameCube'],
+            trailer: ['"#"']
+        }
+        {
+            name: 'Super Mario Sunshine',
+            director: 'Yoshiaki Koizumi & Kenta Usui',
+            releaseYear: 2002,
+            picture: 'link/to/a/picture',
+            platform: ['GameCube'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Super Mario Galaxy',
+            director: 'Yoshiaki Koizumi',
+            releaseYear: 2007,
+            picture: 'link/to/a/picture',
+            platform: ['Nintendo Wii'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Super Mario Odyssey',
+            director: '	Kenta Motokura',
+            releaseYear: 2017,
+            picture: 'link/to/a/picture',
+            platform: ['Nintendo Switch'],
+            trailer: ['"#"']
+        },
+        {
+            name: 'Pulp Fiction',
+            director: '	Koichi Hayashida & Kenta Motokura',
+            releaseYear: 2021,
+            picture: 'link/to/a/picture',
+            platform: ['Nintendo Switch'],
+            trailer: ['"#"']
+        }]
